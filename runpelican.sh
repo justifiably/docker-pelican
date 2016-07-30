77f05ee93238d1c95c6f4dfc3e4638525a6190a8
Justifiably <justifiably@ymail.com>
2016-07-30 12:14:27 +0100

    Use inotifywait to refresh instead of pelican -r, to save CPU

--- runpelican.sh
+++ runpelican.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
-pelican -r /srv/pelican/content -o /srv/pelican/output/ -s pelicanconf.py
+
+# Alternative to "pelican -r"'s polling: use inotify
+while true; do
+    pelican /srv/pelican/content -o /srv/pelican/output/ -s pelicanconf.py
+    inotifywait -qqr --exclude "\#.*|.*~" -e close_write,move,delete,create /srv/pelican/content /srv/pelican/config
+    sleep 3
+    echo "Rerunning pelican on `date`"
+done
